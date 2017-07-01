3dd1142dcf64b7dc10d7214052e0b0eb119d5490
stas <kelen.omnel1@gmail.com>
2017-07-01 23:21:15 +0300

    Did

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-﻿
+﻿function min (arr) {
+	if (arr) {
+    minn = Infinity;
+    arr.forEach(function (elem, index=0, arr) {
+		if (elem < minn && typeof(elem) == 'number')
+		minn=elem; 
+	});
+	if (minn == Infinity) minn = undefined;  
+    return minn;
+	}
+}
+
+function max (arr) {
+	if (arr) {
+	maxx = -Infinity;
+	arr.forEach(function (elem, index=0, arr) {
+	    if (elem > maxx && typeof(elem) == 'number') 
+		maxx=elem;
+	});
+	if (maxx == -Infinity) maxx = undefined;  
+    return maxx;
+	}
+}
+
+
+
+function sum(){
+		var result = 0;
+
+		for (i = 0; 
+		    i < arguments.length;
+		    i += 1){
+			if(typeof(arguments[i]) != 'number'){
+				arguments[i] = 0;
+			}
+			result = result + arguments[i];
+		}
+		return result;
+}
+
+
+
