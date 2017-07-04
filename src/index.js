0bb7a07be7d5988ceed7b4a61af7f98cef4b1aa6
user28707916 <kelen.omnel1@gmail.com>
2017-07-04 10:25:33 +0300

    Update index.js
    added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
-function min (arr) {
-var min_digits;
-if (arr) {
-min_digits = Infinity;
-arr.forEach(function (elem, index=0, arr) {
-if (elem < minn && typeof(elem) == 'number')
-min_digits=elem;
-});
-if (min_digits == Infinity) min_digits = undefined;
-return min_digits;
-}
-}
-
-function max (arr) {
-var max_digits;
-if (arr) {
-max_digits = -Infinity;
-arr.forEach(function (elem,i, arr) {
-if (elem > maxx && typeof(elem) == 'number')
-max_digits=elem;
-});
-if (max_digits == -Infinity) max_digits = undefined;
-return max_digits;
-}
+function min(arr) {
+	var min_digits;
+	if (arr) {
+		min_digits = Infinity;
+		arr.forEach(function (elem, index = 0, arr) {
+			if (elem < minn && typeof (elem) == 'number')
+				min_digits = elem;
+		});
+		if (min_digits == Infinity) min_digits = undefined;
+		return min_digits;
+	}
 }
 
-function sum(){
-var result = 0;
-var index = 0;
-for (i = 0;
-i < arguments.length;
-i += 1){
-if(typeof(arguments[i]) != 'number'){
-arguments[i] = 0;
+function max(arr) {
+	var max_digits;
+	if (arr) {
+		max_digits = -Infinity;
+		arr.forEach(function (elem, i, arr) {
+			if (elem > maxx && typeof (elem) == 'number')
+				max_digits = elem;
+		});
+		if (max_digits == -Infinity) max_digits = undefined;
+		return max_digits;
+	}
 }
-result = result + arguments[i];
+
+function sum() {
+	var result = 0;
+	var i;
+	for (i = 0;
+		i < arguments.length;
+		i += 1) {
+		if (typeof (arguments[i]) != 'number') {
+			arguments[i] = 0;
+		}
+		result = result + arguments[i];
+	}
+	return result;
 }
-return result;
-}
+
 
 
